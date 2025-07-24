4c5b7ce3f2d214c7084aa4b9f2be74bb3c0fa94a
horribleinternet <cyberblah@gmail.com>
2025-07-24 22:45:34 +0900

    add repl loop

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	prompt := "Pokedex > "
+	fmt.Print(prompt)
+	for scanner.Scan() {
+		input := cleanInput(scanner.Text())
+		fmt.Println("Your command was:", input[0])
+		fmt.Print(prompt)
+	}
+}
+
+func cleanInput(text string) []string {
+	text = strings.TrimSpace(text)
+	text = strings.ToLower(text)
+	out := strings.Fields(text)
+	if len(out) == 0 {
+		return []string{""}
+	}
+	return out
 }

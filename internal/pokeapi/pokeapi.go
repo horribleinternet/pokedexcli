17d5660cf7b0baf71bd6509a684277289bd31cdb
horribleinternet <cyberblah@gmail.com>
2025-07-26 03:21:19 +0900

    add listing pokemon at a location

--- internal/pokeapi/pokeapi.go
+++ internal/pokeapi/pokeapi.go
@@ -18,6 +18,73 @@ type locationAreas struct {
 	} `json:"results"`
 }
 
+type locationArea struct {
+	EncounterMethodRates []struct {
+		EncounterMethod struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"encounter_method"`
+		VersionDetails []struct {
+			Rate    int `json:"rate"`
+			Version struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version"`
+		} `json:"version_details"`
+	} `json:"encounter_method_rates"`
+	GameIndex int `json:"game_index"`
+	ID        int `json:"id"`
+	Location  struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"location"`
+	Name  string `json:"name"`
+	Names []struct {
+		Language struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+		Name string `json:"name"`
+	} `json:"names"`
+	PokemonEncounters []struct {
+		Pokemon struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"pokemon"`
+		VersionDetails []struct {
+			EncounterDetails []struct {
+				Chance          int `json:"chance"`
+				ConditionValues []struct {
+					ID        int    `json:"id"`
+					Name      string `json:"name"`
+					Condition struct {
+						Name string `json:"name"`
+						URL  string `json:"url"`
+					} `json:"condition"`
+					Names []struct {
+						Name     string `json:"name"`
+						Language struct {
+							Name string `json:"name"`
+							URL  string `json:"url"`
+						} `json:"language"`
+					} `json:"names"`
+				} `json:"condition_values"`
+				MaxLevel int `json:"max_level"`
+				Method   struct {
+					Name string `json:"name"`
+					URL  string `json:"url"`
+				} `json:"method"`
+				MinLevel int `json:"min_level"`
+			} `json:"encounter_details"`
+			MaxChance int `json:"max_chance"`
+			Version   struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version"`
+		} `json:"version_details"`
+	} `json:"pokemon_encounters"`
+}
+
 const LocationStartURL = "https://pokeapi.co/api/v2/location-area/"
 const cachemsInterval = 5000
 
@@ -25,6 +92,23 @@ var cache *pokecache.Cache
 
 func init() {
 	cache = pokecache.NewCache(cachemsInterval)
+}
+
+func LocationPokemon(area string) ([]string, error) {
+	url := LocationStartURL + area + "/"
+	bytes, err := getBuffer(url)
+	if err != nil {
+		return nil, err
+	}
+	var location locationArea
+	if err := json.Unmarshal(bytes, &location); err != nil {
+		return nil, err
+	}
+	pokemons := make([]string, len(location.PokemonEncounters))
+	for i, encounter := range location.PokemonEncounters {
+		pokemons[i] = encounter.Pokemon.Name
+	}
+	return pokemons, nil
 }
 
 func LocationPage(url string) (locations []string, nextUrl string, prevUrl string, err error) {

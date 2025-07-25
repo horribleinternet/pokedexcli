eebb772a01a886e3dece723548a276c6a75380f1
horribleinternet <cyberblah@gmail.com>
2025-07-25 23:35:33 +0900

    add map commands

--- main.go
+++ main.go
@@ -4,13 +4,19 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"pokedexcli/internal/pokeapi"
 	"strings"
 )
 
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
+}
+
+type config struct {
+	nextURL string
+	prevURL string
 }
 
 var commands map[string]cliCommand
@@ -27,6 +33,16 @@ func initCommands() {
 			description: "Displays a help message",
 			callback:    commandHelp,
 		},
+		"map": {
+			name:        "map",
+			description: "Displays the next 20 locations",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Displays the previous 20 locations",
+			callback:    commandMapb,
+		},
 	}
 }
 
@@ -35,11 +51,12 @@ func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	prompt := "Pokedex > "
 	fmt.Print(prompt)
+	context := config{nextURL: pokeapi.LocationStartURL, prevURL: ""}
 	for scanner.Scan() {
 		input := cleanInput(scanner.Text())
 		comm, ok := commands[input[0]]
 		if ok {
-			if err := comm.callback(); err != nil {
+			if err := comm.callback(&context); err != nil {
 				fmt.Printf("Error: %v", err)
 			}
 		} else {
@@ -49,18 +66,47 @@ func main() {
 	}
 }
 
-func commandExit() error {
+func commandExit(context *config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(context *config) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
 	fmt.Println()
 	for _, value := range commands {
 		fmt.Printf("%s: %s\n", value.name, value.description)
+	}
+	return nil
+}
+
+func commandMap(context *config) error {
+	if context.nextURL == "" {
+		fmt.Println("you're on the last page")
+		return nil
+	}
+	return printMap(context.nextURL, context)
+}
+
+func commandMapb(context *config) error {
+	if context.prevURL == "" {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+	return printMap(context.prevURL, context)
+}
+
+func printMap(url string, context *config) error {
+	locations, nextURL, prevURL, err := pokeapi.LocationPage(url)
+	if err != nil {
+		return err
+	}
+	context.nextURL = nextURL
+	context.prevURL = prevURL
+	for _, location := range locations {
+		fmt.Println(location)
 	}
 	return nil
 }
